3c9a58532424a7d421d215ff867a430859eb4bfd
Annika <a.staalhandske@gmail.com>
2020-11-29 12:14:51 +0100

    added current year

--- project/main.js
+++ project/main.js
@@ -1,1 +1,13 @@
 "use strict";
+
+//Variable year håller constructor function Date() =2020 aktuellt år
+let currentYear = new Date().getFullYear();
+
+//functionen tilldelar värdet i currentYear till id "year" i index.html
+function showCalender() {
+  let yearP = document.getElementById("year");
+  yearP.innerHTML = currentYear;
+}
+
+//Kallar functionen så året blir synligt
+showCalender();
