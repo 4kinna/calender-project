e5cee4a1238938fc844d17d2963df010dbbb13c5
Hamodi Alshaikhli <hamodi.alshaikhli@hotmail.com>
2020-11-30 14:18:39 +0100

    Added months

--- project/main.js
+++ project/main.js
@@ -1,1 +1,24 @@
 "use strict";
+const date = new Date();
+
+let months = [
+    "January",
+    "February",
+    "Mars",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+
+document.querySelector(".date p").innerHTML = date.toDateString();
+
+
+
