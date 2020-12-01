70947b5f618966b08f1eedafe317e25ff738b12e
Hamodi Alshaikhli <hamodi.alshaikhli@hotmail.com>
2020-12-01 11:23:15 +0100

    lagt till kommenatarer

--- project/main.js
+++ project/main.js
@@ -21,18 +21,18 @@ let months = [
     "December",
 ];
 
+//två knappar går fram i tiden eller bakåt, 
+//ta det steg för steg, börja med knappen så att den ändrar i loggen  
+
 document.querySelector(".date h1").innerHTML = months[date.getMonth()];
-
-
-
 
 
 
 
 //functionen tilldelar värdet i currentYear till id "year" i index.html
 function showCalender() {
-  let yearP = document.getElementById("year");
-  yearP.innerHTML = currentYear;
+    let yearP = document.getElementById("year");
+    yearP.innerHTML = currentYear;
 }
 
 //Kallar functionen så året blir synligt
@@ -59,40 +59,39 @@ for (let index = 0; index < days.length; index++) {
 // Räknar ut hur många dagar det finns i denna månad.
 function totalDaysInMonthFunc(monthModified, year) {
     return new Date(year, monthModified, -0).getDate();
-  }
-  
-  // Tar reda på vilken vilket index den första vexkodagen i månaden har, 0=söndag.
-  var day = new Date(currentYear + "-" + monthModified + "-01").getDay();
-  
-  // Om första dagen i månaden är en söndag.
-  if (day === 0) {
+}
+
+// Tar reda på vilken vilket index den första vexkodagen i månaden har, 0=söndag.
+var day = new Date(currentYear + "-" + monthModified + "-01").getDay();
+
+// Om första dagen i månaden är en söndag.
+if (day === 0) {
     for (let index = 0; index < 6; index++) {
-      let newDiv = document.createElement("div");
-      let textInDiv = document.createTextNode(" ");
-      newDiv.appendChild(textInDiv);
-      let element = document.getElementById("days-number-id");
-      element.appendChild(newDiv);
+        let newDiv = document.createElement("div");
+        let textInDiv = document.createTextNode(" ");
+        newDiv.appendChild(textInDiv);
+        let element = document.getElementById("days-number-id");
+        element.appendChild(newDiv);
     }
-  } else {
+} else {
     for (let index = day; index > 1; index--) {
-      let newDiv = document.createElement("div");
-      let textInDiv = document.createTextNode(" ");
-      newDiv.appendChild(textInDiv);
-      let element = document.getElementById("days-number-id");
-      element.appendChild(newDiv);
+        let newDiv = document.createElement("div");
+        let textInDiv = document.createTextNode(" ");
+        newDiv.appendChild(textInDiv);
+        let element = document.getElementById("days-number-id");
+        element.appendChild(newDiv);
     }
-  }
-  
-  // Skapar månadens alla dagar (nummer)
-  for (
+}
+
+// Skapar månadens alla dagar (nummer)
+for (
     let index = 1;
     index <= totalDaysInMonthFunc(monthModified, currentYear);
     index++
-  ) {
+) {
     let newDiv = document.createElement("div");
     let textInDiv = document.createTextNode([index]);
     newDiv.appendChild(textInDiv);
     let element = document.getElementById("days-number-id");
     element.appendChild(newDiv);
-  }
-  
+}
